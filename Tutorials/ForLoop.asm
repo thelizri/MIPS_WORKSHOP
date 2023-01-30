db5a2cc3d85d2527d87a3cfa2008b599017ec6e7
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-30 01:11:21 +0100

    Update

---
+++ Tutorials/ForLoop.asm
@@ -0,0 +1,17 @@
+.text
+li $t0, 0 			#i=0
+forLoop:			#Create a label for the for loop. 
+#Place code between here
+#This code prints 'Hello world\n'. Replace with your code. 
+la $a0, string
+li $v0, 4
+syscall
+#Place code between here
+addi $t0, $t0, 1  		#i++
+slti $t1, $t0, 10 		#$t1 = 1 if i<10
+bne $t1, $zero, forLoop		#go to forLoop if i<10
+
+li $v0, 10			#Syscall to exit
+syscall
+.data
+string: .asciiz "Hello world\n"
