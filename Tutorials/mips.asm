a4267c3d886cdede2e04cce66f337e655d6b9892
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-31 23:16:56 +0100

    update

--- Tutorials/mips.asm
+++ Tutorials/mips.asm
@@ -1,32 +1,49 @@
-#Stack grows downwards.
-#Heap grows upwards. 
-#If we're calling a subprogram within a subprogram, we need to push
-#$ra to the stack. So we don't overwrite our return address
-	.text
+#Since we don't know how big the string the user is gonna enter is, we need to allocate some dynamic memory.
+.text
 main:
-	jal GoodSubprogram
-	nop
-	la $a0, string3
-	jal PrintString
-	nop
-	jal Exit
-	nop
-GoodSubprogram:
-	addi $sp, $sp, -4 # save space on the stack (push) for the $ra
-	sw $ra, 0($sp) # save $ra
-	la $a0, string1
-	jal PrintString
-	nop
-	li $v0, 4
-	la $a0, string2
-	syscall
-	lw $ra, 0($sp) # restore $ra
-	addi $sp, $sp, 4 # return the space on the stack (pop)
-	jr $ra
-	
-	.data
-string1: .asciiz "\nIn subprogram GoodExample\n"
-string2: .asciiz "After call to PrintString\n"
-string3: .asciiz "After call to GoodExample\n"
+la $a0, prompt1 # Read and print first string
+li $a1, 80      #Number of bytes to allocate in memory
+jal PromptString
+nop
+move $a0, $v0
+jal PrintString
+nop
+la $a0, prompt2 # Read and print second string
+li $a1, 80      #Number of bytes to allocate in memory
+jal PromptString
+nop
+move $a0, $v0
+jal PrintString
+nop
+jal Exit
+nop
+.data
+prompt1: .asciiz "Enter the first string: "
+prompt2: .asciiz "Enter the second string: "
+.text
+PromptString:
+addi $sp, $sp, -12 # Push stack
+sw $ra, 0($sp)
+sw $a1, 4($sp)
+sw $s0, 8($sp)
+li $v0, 4          # Print the prompt
+syscall
+
+li $v0, 9          #Syscall for allocating memory. $a0 = number of bytes to allocate.
+lw $a0, 4($sp)     #Load the number 80 into $a0. 
+syscall
+move $s0, $v0      # Move the address of the allocated memory to $s0
+move $a0, $s0      # Read the string. Move memory address to $a0. 
+
+li $v0, 8	   # Syscall for reading a string. $a0 = address of input buffer	
+lw $a1, 4($sp)     # $a1 = maximum number of characters to read. We're loading the number 80 into $a1.
+syscall
+
+move $v0, $s0     # Save string address to return
+lw $ra, 0($sp)    # Pop stack
+lw $s0, 8($sp)
+addi $sp, $sp, 12
+jr $ra
+nop
 
 .include "utils.asm"
