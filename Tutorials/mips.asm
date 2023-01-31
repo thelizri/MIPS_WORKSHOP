aa5b850c35d2c2fa5355ebbfa60b94cbfa2a1a3e
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-01-31 16:50:43 +0100

    Update

--- Tutorials/mips.asm
+++ Tutorials/mips.asm
@@ -1,13 +1,25 @@
+.macro Exit 		#Name of macro
+#Define macro here
+li $v0,10
+syscall
+#Define macro here
+.end_macro	
+.macro PrintHello 	#Name of macro.
+la $a0, hello
+li $v0, 4
+syscall
+.end_macro
 .text
+PrintHello
 la $a0, string
 jal PrintString #Print string located at address $a0
 nop
 lw $a0, number
 jal PrintInteger #Print integer stored in $a0
 nop
-jal Exit #Exits the program
-nop
+Exit #call macro. Code that exists in macro will replace the label. 
 .data
 number: .word 69
 string: .asciiz "Number: "
+hello: .asciiz "Hello user. Welcome! \n"
 .include "utils.asm" #Imports functions from our utils.asm file. Make sure to put at the end. 
