d165f83b53cee9e9e4405a20c059c3f8548d81b9
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-01 09:57:47 +0100

    Update

--- Tutorials/Misc/Array.asm
+++ Tutorials/Misc/Array.asm
@@ -1,19 +1,19 @@
 	.text
-addi $s0, $zero, 4
-addi $s1, $zero, 10
-addi $s2, $zero, 20
-
-sw $s0, myArray
-sw $s1, myArray + 4
-sw $s2, myArray + 8
-
-lw $a0, myArray + 8 #Loading word from myArray[2]
-addi $v0, $zero, 1
-syscall
-
+	addi $s0, $zero, 4
+	addi $s1, $zero, 10
+	addi $s2, $zero, 20
+	
+	sw $s0, myArray
+	sw $s1, myArray + 4
+	sw $s2, myArray + 8
+	
+	lw $a0, myArray + 8 #Loading word from myArray[2]
+	addi $v0, $zero, 1
+	syscall
+	
 exit:
 	addi $v0, $zero, 10
 	syscall
-
+	
 	.data
 myArray: .space 12 #reserving space for 12 bytes, in other words 3 integers
--- Tutorials/Misc/Multiplication.asm
+++ Tutorials/Misc/Multiplication.asm
@@ -1,39 +1,39 @@
-.data
+	.data
 prompt: .asciiz "Enter number: \n"
 result: .asciiz "Result: "
-.text
-jal getNumber
-nop
-move $t0, $v0
-
-jal getNumber
-nop
-move $t1, $v0
-
-mult $t1, $t0
-mflo $a1
-
-jal printInteger
-nop
-
+	.text
+	jal getNumber
+	nop
+	move $t0, $v0
+	
+	jal getNumber
+	nop
+	move $t1, $v0
+	
+	mult $t1, $t0
+	mflo $a1
+	
+	jal printInteger
+	nop
+	
 exit:
-li $v0, 10
-syscall
-
+	li $v0, 10
+	syscall
+	
 getNumber:
-addi $v0, $zero, 4
-la $a0, prompt
-syscall #Prints prompt
-addi $v0, $zero, 5
-syscall #Retrieves number from user
-jr $ra
-nop
-
+	addi $v0, $zero, 4
+	la $a0, prompt
+	syscall #Prints prompt
+	addi $v0, $zero, 5
+	syscall #Retrieves number from user
+	jr $ra
+	nop
+	
 printInteger:
-addi $v0, $zero, 4
-la $a0, result
-syscall 
-move $a0, $a1
-addi $v0, $zero, 1 
-syscall #Prints integer
-jr $ra
+	addi $v0, $zero, 4
+	la $a0, result
+	syscall
+	move $a0, $a1
+	addi $v0, $zero, 1
+	syscall #Prints integer
+	jr $ra
