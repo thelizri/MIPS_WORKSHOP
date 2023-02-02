99a7cb39b6794f96f45b889f7d7bf0b439589473
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-02-02 11:55:45 +0100

    Update

--- Tutorials/ForLoop.asm
+++ Tutorials/ForLoop.asm
@@ -1,5 +1,9 @@
+	.data
+string: .asciiz "Hello world\n"
+.eqv SYS_EXIT 10
+.eqv LOOP_COUNTER $t0
 	.text
-	li $t0, 0 #We intialize our loop counter to zero. i=0
+	li LOOP_COUNTER, 0 #We intialize our loop counter to zero. i=0
 forLoop: #Create a label for the for loop.
 	#Place code between here
 	#This code prints 'Hello world\n'. Replace with your code.
@@ -7,12 +11,10 @@ forLoop: #Create a label for the for loop.
 	li $v0, 4
 	syscall
 	#Place code between here
-	addi $t0, $t0, 1 #i + + 
-	slti $t1, $t0, 10 #$t1 = 1 if i<10
-	bne $t1, $zero, forLoop #go to forLoop if i<10
+	addi LOOP_COUNTER, LOOP_COUNTER, 1 #Increment our loop counter by 1 
+	blt LOOP_COUNTER, 5, forLoop #go to forLoop if i<10
 	
-	li $v0, 10 #Syscall to exit
+	li $v0, SYS_EXIT #Syscall to exit
 	syscall
-	
-	.data
-string: .asciiz "Hello world\n"
+
+
